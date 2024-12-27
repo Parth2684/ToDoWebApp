e1b44abcd5591ed690316d526ca7b34be5a9eb61
Parth2684 <bhosle6006@gmail.com>
2024-12-27 20:00:50 +0530

    A Simple Frontend is added

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors")
 const { createTodo, updateTodo } = require("./types");  
 const { todo } = require("./db");
 const app = express();
 const port = 3000;
 
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 
 app.post("/todo", async (req, res)=>{
     const createPayload = req.body;
@@ -14,9 +18,9 @@ app.post("/todo", async (req, res)=>{
             msg: "Wrong Inputs"
         });return
     }
-    await todo.Create({
-        title: parsedPayload.title,
-        description: parsedPayload.description,
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
         completed: false
     })
     res.json({
@@ -27,7 +31,7 @@ app.post("/todo", async (req, res)=>{
 app.get("/todos", async (req, res)=>{
     const response = await todo.find({})
     res.json({
-        todos: express.response
+        response
     })
 });
 
@@ -40,7 +44,7 @@ app.put("/completed", async (req, res)=>{
         });
         return;
     }
-    await todo.update({
+    await todo.updateOne({
         _id: req.body.id
     }, {
         completed: true
